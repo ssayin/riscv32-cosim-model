4a7ed2393340848130fa8dd102e34a59121d97f7
Serdar Sayın <serdarsayin@pm.me>
2023-03-07 15:06:29 +0300

    initial commit

---
+++ test.sv
@@ -0,0 +1,26 @@
+
+module test (
+    input reg clk
+);
+  import "DPI-C" function void decode(
+    input  logic [31:0] word,
+    output logic [31:0] out
+  );
+  import "DPI-C" function void init();
+
+  logic [31:0] res;
+
+  logic [31:0] in;
+
+  always #10 clk = ~clk;
+
+  initial begin
+    init();
+    for (integer i = 0; i < 12; ++i) begin
+      in = $urandom;
+      decode(in, res);
+      $display("%d - %d", in, res);
+    end
+    $finish;
+  end
+endmodule : test
