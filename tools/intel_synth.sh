b721e2f8ac98d088b01221c3daa1bebd2300c1b5
Serdar Sayın <serdarsayin@pm.me>
2023-08-08 14:36:16 +0300

    housekeeping: move scripts to tools/

--- tools/intel_synth.sh
+++ tools/intel_synth.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Exit immediately after failure
+set -e
+
 # For synthesizing on Quartus Lite Software
 quartus_sh -t tools/fpga_top.tcl compile fpga_top rev_1
 quartus_map fpga_top 
 quartus_fit fpga_top
 quartus_asm fpga_top
 quartus_sta fpga_top
-# quartus_sim fpga_top
