4d095e50f856286186555db31372d33d1273804c
Serdar Sayın <serdarsayin@pm.me>
2023-08-12 06:57:44 +0300

    refactor: cleanup

--- src/dpi/decodesv.cpp
+++ src/dpi/decodesv.cpp
@@ -29,35 +29,23 @@ void dpi_decoder_input(const decoder_in_t *in) {}
 
 void dpi_decoder_output(decoder_out_t *out) {}
 
-void dpi_decoder_process(const decoder_in_t *in, decoder_out_t *out) {
+inline void init_output(decoder_out_t *out) {
   memset(out, 0, sizeof(decoder_out_t));
-  printf("HI\n");
+}
 
-  uint32_t instr = in->instr.aval;
-  bool is_compressed = (instr & 0x3) != 0x3;
-  op dec;
-  if (is_compressed) {
-    dec = decode16(instr & 0x0000FFFF);
-  } else {
-    dec = decode(instr);
-  }
-
+void set_imm_and_registers(const op &dec, decoder_out_t *out) {
   out->use_imm = dec.has_imm;
-
   EXPORTER_VECVAL_SET(out->imm, dec.imm);
   EXPORTER_VECVAL_SET(out->rd_addr, dec.rd);
   EXPORTER_VECVAL_SET(out->rs1_addr, dec.rs1);
   EXPORTER_VECVAL_SET(out->rs2_addr, dec.rs2);
+}
 
-  // memset(&out->pt, 0, sizeof(pt_t));
-  out->lsu = 0;
-  out->alu = 0;
-  out->br = 0;
-  out->illegal = 0;
-
+void set_flags(const op &dec, bool is_compressed, decoder_out_t *out,
+               uint32_t instr) {
   if (dec.tgt == target::alu) {
-    alu alu0 = std::get<alu>(dec.opt);
-    if ((alu0 == alu::_jal) || (alu0 == alu::_jalr)) {
+    if ((std::get<alu>(dec.opt) == alu::_jal) ||
+        (std::get<alu>(dec.opt) == alu::_jalr)) {
       out->jal = 1;
     }
     out->alu = 1;
@@ -66,7 +54,6 @@ void dpi_decoder_process(const decoder_in_t *in, decoder_out_t *out) {
          static_cast<uint32_t>(masks::opcode::auipc))) {
       out->auipc = 1;
     }
-
     if ((instr & static_cast<uint32_t>(masks::opcode::lui)) ==
             static_cast<uint32_t>(masks::opcode::lui) ||
         (is_compressed && (instr & 0b0110000000000001) == 0b0110000000000001)) {
@@ -75,14 +62,13 @@ void dpi_decoder_process(const decoder_in_t *in, decoder_out_t *out) {
     if ((instr & static_cast<uint32_t>(masks::opcode::jal)) ==
         static_cast<uint32_t>(masks::opcode::jal)) {
       out->jal = 1;
-      // 32'b????????????????011???????????01
     }
   }
+}
 
+void set_target_flags(const op &dec, decoder_out_t *out) {
   switch (dec.tgt) {
   case target::load:
-    out->lsu = 1;
-    break;
   case target::store:
     out->lsu = 1;
     break;
@@ -95,10 +81,20 @@ void dpi_decoder_process(const decoder_in_t *in, decoder_out_t *out) {
   case target::csr:
     out->csr = 1;
     break;
-  case target::alu:
-  case target::mret:
-  case target::ebreak:
-  case target::ecall:
+  default:
     break;
   }
+}
+
+void dpi_decoder_process(const decoder_in_t *in, decoder_out_t *out) {
+  init_output(out);
+
+  uint32_t instr = in->instr.aval;
+  bool is_compressed = (instr & 0x3) != 0x3;
+
+  op dec = is_compressed ? decode16(instr & 0x0000FFFF) : decode(instr);
+
+  set_imm_and_registers(dec, out);
+  set_flags(dec, is_compressed, out, instr);
+  set_target_flags(dec, out);
 }
--- src/tb/uvm_bfm/tb/bfm_test/sv/bfm_test_pkg.sv
+++ src/tb/uvm_bfm/tb/bfm_test/sv/bfm_test_pkg.sv
@@ -13,7 +13,7 @@
 //
 // Version:   0.1
 //
-// Code created by Easier UVM Code Generator version 2017-01-19 on Sat Aug 12 06:44:54 2023
+// Code created by Easier UVM Code Generator version 2017-01-19 on Sat Aug 12 06:57:02 2023
 //=============================================================================
 // Description: Test package for bfm
 //=============================================================================
